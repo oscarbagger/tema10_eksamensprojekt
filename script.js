2c6bd2ff76acde57fdec2ffe60e2447f16e3a9c7
oscarbagger <oscarbagger@live.dk>
2019-12-09 01:24:48 +0100

    setup site

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", Start);
+
+let content=[];
+const footerUrl = "";
+const navUrl = "";
+const pageURl = "https://oscarbagger.dk/kea/10_Eksamensprojekt/wordpress/wp-json/wp/v2/pages/?&per_page=99";
+
+
+function Start()
+{
+    
+}
