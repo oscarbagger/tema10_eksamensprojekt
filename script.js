7e91268373c54c1ee3e7ebbd37166fa22d24283c
oscarbagger <oscarbagger@live.dk>
2019-12-12 23:55:59 +0100

    html setup

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 window.addEventListener("DOMContentLoaded", Start);
 
 let content=[];
+const header=document.querySelector("header");
 const footerUrl = "";
 const navUrl = "";
-const pageURl = "https://oscarbagger.dk/kea/10_Eksamensprojekt/wordpress/wp-json/wp/v2/pages/?&per_page=99";
+//const pageURl = "https://oscarbagger.dk/kea/10_Eksamensprojekt/wordpress/wp-json/wp/v2/pages/?&per_page=99";
 
+
+let menuIsOpen = false;
 
 function Start()
 {
-    
+    MakeBurgerMenu();
+}
+
+
+function MakeBurgerMenu() {
+    let items = header.querySelector(".menuItems");
+    let menu = header.querySelector(".burger");
+    menu.addEventListener("click", () => {
+        if (menuIsOpen == false) {
+            menuIsOpen = true;
+            items.style.display = "flex";
+            //menu.querySelector("img").src = src = "images/burger_close.png";
+        } else {
+            menuIsOpen = false;
+            items.style.display = "none";
+            //menu.querySelector("img").src = src = "images/iconmonstr-menu-1.svg";
+        }
+    })   
 }
