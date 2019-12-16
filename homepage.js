277177875a096485675cad0e8d9284659fd03fab
oscar <oscarbagger@live.dk>
2019-12-16 14:50:34 +0100

    align align align

---
+++ homepage.js
@@ -0,0 +1,45 @@
+window.addEventListener("DOMContentLoaded", Start);
+
+
+let splashList=[];
+const homepageUrl = "https://oscarbagger.dk/kea/10_Eksamensprojekt/wordpress/wp-json/wp/v2/homepage/";
+
+let splashIndex=0;
+let splashAmount;
+let timer=5000;
+
+const splashImg=document.querySelector(".currentSplash");
+const prevSplash=document.querySelector(".prevSplash");
+
+function Start()
+{
+    GetSplash();
+}
+
+async function GetSplash()
+{
+    const response = await fetch(homepageUrl);
+    let homepageList = await response.json();
+    splashList=homepageList[0].splash;
+    console.log(splashList);
+    splashAmount=splashList.length;
+    // start udskiftning
+    setTimeout(ChangeSplash, timer);
+}
+
+function ChangeSplash() {
+    splashImg.classList.remove("transitionIn");
+    prevSplash.src=splashList[splashIndex].guid;
+    prevSplash.onload=() => {
+        if (splashIndex+1<splashAmount)
+        {splashIndex++}
+        else {
+            splashIndex=0;
+        }
+        splashImg.src=splashList[splashIndex].guid;
+        splashImg.onload= () => {
+            splashImg.classList.add("transitionIn");
+        }
+        setTimeout(ChangeSplash, timer);
+    }
+}
